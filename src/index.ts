382a3662fe6e49aff95302b29a52d9b655a60704
Lim Diat Bbin <limdiatbbin@Lims-MacBook-Air-2.local>
2025-06-20 01:29:36 +0800

    Allow videos to be added to firestore

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import {
     deleteProcessedVideo, 
     uploadProcessedVideo
 } from './storage';
+import { isVideoNew, setVideo } from './firestore';
 
 const app = express();
 app.use(express.json());
@@ -27,6 +28,17 @@ app.post('/process-video', async (req, res) => {
 
     const inputFileName = data.name;
     const outputFileName = `processed-${inputFileName}`;
+    const videoId = inputFileName.split('.')[0];
+
+    if (!isVideoNew(videoId)) {
+        return res.status(400).send('Bad request: video already processing or processed');
+    } else {
+        await setVideo(videoId, {
+            id: videoId,
+            uid: videoId.split("-")[0],
+            status: "processing"
+        })
+    }
 
     await setUpDirectories();
     
@@ -46,6 +58,11 @@ app.post('/process-video', async (req, res) => {
 
     // Uplaod the processed video to cloud storage
     await uploadProcessedVideo(outputFileName)
+
+    await setVideo(videoId, {
+        status: "processed",
+        filename: outputFileName
+    })
 
     await Promise.all([
         deleteRawVideo(inputFileName),
