0ae269004d62744ec63ae3b1167f9309e2e158ad
Lim Diat Bbin <limdiatbbin@Lims-MacBook-Air-2.local>
2026-04-12 22:47:29 +0800

    Update firebase config

--- yt-web-client/app/firebase/firebase.ts
+++ yt-web-client/app/firebase/firebase.ts
@@ -14,12 +14,15 @@ import { getFunctions } from "firebase/functions";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Next.js only exposes env vars to the browser when prefixed with NEXT_PUBLIC_.
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: "clone-2d9c6.firebaseapp.com",
-  projectId: "clone-2d9c6",
-  appId: "1:296066166592:web:77bfb1cf6c5ba6d9c414a5",
-  measurementId: "G-ZMRKHF8H1K"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 // Initialize Firebase
