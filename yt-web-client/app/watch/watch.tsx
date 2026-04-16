9bc0257630ad16efe17de202d85504d35bf3231b
Diatbbin <l_db_@hotmail.com>
2026-04-16 22:44:53 +0800

    Update watch page views

--- yt-web-client/app/watch/watch.tsx
+++ yt-web-client/app/watch/watch.tsx
@@ -1,15 +1,27 @@
 "use client";
+import styles from './watch.module.css';
 
 import { useSearchParams } from 'next/navigation'
 
 export default function Watch() {
-  const videoPrefix = 'https://storage.googleapis.com/processed-videos-db/';
-  const videoSrc = useSearchParams().get('v');
+    const videoPrefix = 'https://storage.googleapis.com/processed-videos-db/';
+    const videoSrc = useSearchParams().get('v');
+    const selectedVideo = videoPrefix + videoSrc
 
-  return (
-    <div>
-      <h1>Watch Page</h1>
-      { <video controls src={videoPrefix + videoSrc}/> }
-    </div>
-  );
+    return (
+        <div>
+            <main className={styles.watchPage}>
+              <section className={styles.headerSection}>
+                  <h1 className={styles.heading}>Watch video</h1>
+                  <p className={styles.subheading}>Stream your selected video!</p>
+              </section>
+
+              <section className={styles.playerSection}>
+                  <h2 className={styles.playerTitle}>Now playing</h2>
+             
+                  {selectedVideo && <video className={styles.videoPlayer} controls src={selectedVideo} />}
+              </section>
+            </main>
+        </div>
+    );
 }
